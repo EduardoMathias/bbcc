499a79e814d4dd48740571a10f9c3e4ac9d44ada
EduardoMathias <eduardo.souza@ort.org.br>
2019-11-08 15:03:35 +0000

    Simulacao

--- simulacao.c
+++ simulacao.c
@@ -2,12 +2,34 @@
 #include <stdlib.h>
 #include "lib_fila.h"
 
+int escolhaAterrizagem(int t1, int t2, int t3, int t4){
+    if(t1 < t2 && t1 < t3 && t1 < t4)
+        return 1;
+    if(t2 < t1 && t2< t3 && t2 < t4)
+        return 2;
+    if (t3< t1 && t3 < t2 && t3 < t4)
+        return 3;
+    if (t4 < t1 && t4 < t2 && t4 < t3)
+        return 4;
+    return 0;
+}
+
+int escolhaDecola(int t1, int t2, int t3){
+    if(t1 < t2 && t1 < t3)
+        return 1;
+    if(t2 < t1 && t2< t3)
+        return 2;
+    if (t3< t1 && t3 < t2)
+        return 3;
+    return 0;
+}
+
 int main(int argc, char **argv){
     int idpar,idimpar;
     idpar = 2;
     idimpar = 1;
     int Ttotal,QntdAterrizagem,QntdDecolagem,j;
-    t_fila aterissagem1,aterissagem2,aterissagem3,aterissagem4,decolagem1,decolagem2,decolagem3; 
+    t_fila aterissagem1,aterissagem2,aterissagem3,aterissagem4,decolagem1,decolagem2,decolagem3;
     inicializa_fila(&aterissagem1);
     inicializa_fila(&aterissagem2);
     inicializa_fila(&aterissagem3);
@@ -19,12 +41,26 @@ int main(int argc, char **argv){
     for(int i= 0; i< Ttotal; i++){
         scanf("%d", &QntdAterrizagem);
         for(int j = 0; j < QntdAterrizagem;j++){
-            enfileira(idimpar,j,&aterrisagem1);
+            int escolha = escolhaAterrizagem(tamanho(&aterissagem1), tamanho(&aterissagem2), tamanho(&aterissagem3), tamanho(&aterissagem4));
+            if(escolha == 1)
+                enfileira(idimpar,j,&aterissagem1);
+            if(escolha == 2)
+                enfileira(idimpar,j, &aterissagem2);
+            if(escolha == 3)
+                enfileira(idimpar,j, &aterissagem3);
+            if(escolha == 4)
+                enfileira(idimpar,j,&aterissagem4);
             idimpar+=2;
         }
         scanf("%d", &QntdDecolagem);
         for(int j= 0; j < QntdDecolagem; j++){
-            enfileira(idpar,j, &decolagem1);
+            int escolha_decola = escolhaDecola(tamanho(&decolagem1), tamanho(&decolagem2), tamanho(&decolagem3));
+            if(escolha_decola == 1)
+                enfileira(idpar,j, &decolagem1);
+            if(escolha_decola == 2)
+                enfileira(idpar,j,&decolagem2);
+            if(escolha_decola == 3)
+                enfileira(idpar,j,&decolagem3);
             idpar+=2;
         }
     }
