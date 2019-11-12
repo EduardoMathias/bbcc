d04a86b5d67adfbeace65e1dfeee2655d526e649
EduardoMathias <eduardo.souza@ort.org.br>
2019-11-12 14:06:34 +0000

    Atualizando spaceInvaders

--- Trab_2/elem.c
+++ Trab_2/elem.c
@@ -1,4 +1,61 @@
 #include "elem.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int inicializa_lista(t_lista *l){
+    l->tamanho = 0;
+	l->ini = NULL;
+    l->atual = NULL;
+	return 1;
+}
+
+int insere_inicio_lista(int tipo, int lin, int col, int velocidade, int condicao, t_lista *l)
+{
+	t_nodo *novo;
+
+	novo = (t_nodo*) malloc(sizeof(t_nodo));
+	if (novo == NULL)
+	{
+		free(novo);
+		return 0;
+	}
+
+	novo->tipo = tipo;
+	novo->x = lin;
+	novo->y = col;
+	novo->velocidade = velocidade;
+	novo->condicao = condicao;
+	novo->prox = l->ini;
+    l->ini = novo;
+	l->tamanho++;
+	return 1;
+}
+
+int insere_fim_lista(int tipo, int lin, int col, int velocidade, int condicao, t_lista *l){
+    t_nodo *novo,*aux;
+	novo = malloc(1*sizeof(t_nodo));
+	if (novo == NULL)
+        return 0;
+    novo->tipo = tipo;
+	novo->x = lin;
+	novo->y = col;
+	novo->velocidade = velocidade;
+	novo->condicao = condicao;
+	novo->prox = NULL;
+	if(l->ini){
+		aux = l->ini;
+		while(aux->prox != NULL){
+			aux = aux->prox;
+		}
+		aux->prox = novo;
+	}
+	else
+		l->ini = novo;
+	l->tamanho++;
+	return 1;
+}
+
 
 Nave inicializaNave(int x, int y){
 	Nave NovaNave = {
--- Trab_2/elem.h
+++ Trab_2/elem.h
@@ -2,47 +2,24 @@
 
 struct t_nodo {
     int tipo;
-    int local;
+    int x,y;
     int velocidade;
-    int estado;
+    int condicao;
     struct t_nodo *prox;
 };
 typedef struct t_nodo t_nodo;
 
-struct t_lista_Aliens {
-    t_nodo *alien;
+struct t_lista {
+    t_nodo *ini;
+    t_nodo *atual;
     int tamanho;
 };
-typedef struct t_lista_Aliens t_lista_Aliens;
+typedef struct t_lista t_lista;
 
-struct t_lista_Barreiras{
-    t_nodo *barreira;
-    int tamanho;
-};
-typedef struct t_lista_Barreiras t_lista_Barreiras;
-
-struct t_lista_Nave{
-    t_nodo *nave;
-    int tamanho;
-};
-typedef struct t_lista_Nave t_lista_Nave;
-
-struct t_lista_NaveMae{
-    t_nodo *navemae;
-    int tamanho;
-};
-typedef struct t_lista_NaveMae t_lista_NaveMae;
-
-struct t_lista_Missel{
-    t_nodo *tiro;
-    int tamanho;
-};
-typedef struct t_lista_Missel t_lista_Missel;
-
-int cria_Aliens(t_lista_Aliens *Alien);
-int cria_Barreiras(t_lista_Barreiras *Barreira);
-int cria_Nave(t_lista_Nave *Nave);
-int cria_Mae(t_lista_NaveMae *NaveMae);
+int inicializa_lista(t_lista *l);
+int insere_inicio_lista(int tipo, int lin, int col, int velocidade, int condicao, t_lista *l);
+int insere_fim_lista(int tipo, int lin, int col, int velocidade, int condicao, t_lista *l);
+int consulta_item_atual(int *tipo, int *lin, int *col, int *velocidade, int *condicao, t_lista *l);
 
 typedef struct {
 	char cima[6];
