6c36a013c8d60b9eab1b1711d097abc0348e6b97
EduardoMathias <eduardo.souza@ort.org.br>
2020-03-10 22:56:32 +0000

    1

--- Pasta/ortografia.c
+++ Pasta/ortografia.c
@@ -3,27 +3,29 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, int **argv){
+int main(int argc, char **argv){
 
     FILE* dic; 
     dic = fopen("brazilian", "r"); 
     lista_palavras *palavras;
-    char buffer[];
+    char buffer[20];
     int newSize = 0;
+
+    size_t contagem_palavras = carrega_dic(dic, palavras);
+    /*
     //Cria Lista_palavras
     palavras->palavra = malloc(10 * sizeof(char *));
     palavras->tamanho_usado = 0;
     palavras->tamanho_total = 10;//INICIAL_SIZE
 
     //EXPANSAO PALAVRAS
-    buffer[palavras->tamanho_inicial] = malloc(strlen(buffer) + 1);
-    strcpy(palavras->palavra[palavras->tamanho_inicial], buffer);
-    palavras->tamanho_inicial++;
+    
+    strcpy(palavras->palavra[palavras->tamanho_usado], buffer);
+    palavras->tamanho_usado++;
         if (palavras->tamanho_usado == palavras->tamanho_total){
             newSize = palavras->tamanho_total* 2;
         }
     //EXPANSAO DE LINHAS
     palavras->palavra = realloc(palavras->palavra, newSize * sizeof(char *));
     palavras->tamanho_total = newSize;    
-    }
-}
+    */}
